8bf292ca49908d16cbdd83ffb6cf68b305992a28
Аркадий Федоров <xexex98@MacBook-Pro-Arkadij.local>
2023-04-23 13:58:42 +0300

    up

--- src/Post.jsx
+++ src/Post.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 import { EditPostItem } from './EditPostItem';
 
 export const Post = (props) => {
-  const { name, age, id } = props.item;
+  const { id } = props.item;
   const { setList } = props;
-
   return (
     <div
       style={{ border: '1px solid red', marginBottom: '5px', padding: '5px' }}
     >
-      <EditPostItem id={id} field={'name'} value={name} setList={setList} />
-      <EditPostItem id={id} field={'age'} value={age} setList={setList} />
+      {Object.keys(props.item).map((key, index) => {
+        if (key === 'id') {
+          return null;
+        }
+        return (
+          <EditPostItem
+            key={index}
+            id={id}
+            field={key}
+            value={props.item[key]}
+            setList={setList}
+          />
+        );
+      })}
     </div>
   );
 };
